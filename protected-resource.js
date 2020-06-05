86992b895c616add8298041389d2cfb54030209a
Daniel Abramson <daniel.abramson@ruralsource.com>
2020-06-05 15:57:18 -0600

    Completed module 2

--- protected-resource.js
+++ protected-resource.js
@@ -1,44 +1,82 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const fs = require("fs")
-const { timeout } = require("./utils")
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const { timeout } = require("./utils");
+const jwt = require("jsonwebtoken");
 
 const config = {
-	port: 9002,
-	publicKey: fs.readFileSync("assets/public_key.pem"),
-}
+  port: 9002,
+  publicKey: fs.readFileSync("assets/public_key.pem"),
+};
 
 const users = {
-	user1: {
-		username: "user1",
-		name: "User 1",
-		date_of_birth: "7th October 1990",
-		weight: 57,
-	},
-	john: {
-		username: "john",
-		name: "John Appleseed",
-		date_of_birth: "12th September 1998",
-		weight: 87,
-	},
-}
+  user1: {
+    username: "user1",
+    name: "User 1",
+    date_of_birth: "7th October 1990",
+    weight: 57,
+  },
+  john: {
+    username: "john",
+    name: "John Appleseed",
+    date_of_birth: "12th September 1998",
+    weight: 87,
+  },
+};
 
-const app = express()
-app.use(timeout)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+const app = express();
+app.use(timeout);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-Your code here
-*/
+app.get("/user-info", (req, res) => {
+  if (req.headers.authorization) {
+    let payload = req.headers.authorization.slice(7);
+    let token;
+    jwt.verify(
+      payload,
+      config.publicKey,
+      {
+        algorithms: ["RS256"],
+      },
+      (err, decoded) => {
+        if (err) {
+          res.status(401).send();
+          return;
+        }
+        token = decoded;
+      }
+    );
+
+    if (token) {
+      let scopes = token.scope.split(" ");
+
+      let permissions = [];
+      scopes.forEach((scope) => {
+        permissions.push(scope.slice(11));
+      });
+
+      console.log(permissions);
+      let response = {};
+
+      permissions.forEach((permission) => {
+        response[permission] = users[token.userName][permission];
+      });
+
+      res.json(response);
+    }
+  } else {
+    res.status(401).send();
+  }
+});
 
 const server = app.listen(config.port, "localhost", function () {
-	var host = server.address().address
-	var port = server.address().port
-})
+  var host = server.address().address;
+  var port = server.address().port;
+});
 
 // for testing purposes
 module.exports = {
-	app,
-	server,
-}
+  app,
+  server,
+};
